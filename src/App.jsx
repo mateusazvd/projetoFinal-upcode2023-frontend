743e9d60c20e971b2f83e823532a782db2b53fa7
IsaacEmanoel <104519098+IsaacEmanoel@users.noreply.github.com>
2023-05-17 17:28:22 -0300

    componentização

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import Header from './components/Header';
 import './App.css';
 import Main from './components/Main/Index';
 import Menu from './components/Menu';
+import MainResume from './components/Resume/NameJojas/MainResume';
 
 function App() {
   return (
     <div className="App">
+      
       <Header />
       <Menu />
       <Main />
-
+      <MainResume/>
     </div>
   );
 }
