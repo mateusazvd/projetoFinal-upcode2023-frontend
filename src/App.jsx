0058321e6e8f444dbf15f7a2927b83e66607306c
IsaacEmanoel <104519098+IsaacEmanoel@users.noreply.github.com>
2023-05-17 08:26:54 -0300

    colorfig

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-
 import Header from './components/Header';
+import ResumePage from './components/Resume/ResumePage';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ResumePage />
     </div>
   );
 }
