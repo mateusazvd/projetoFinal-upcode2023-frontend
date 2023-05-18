4df6bb6415abdb4753e07bd713ee74527a5500a8
carlosXavierrr <eduardoxavier2711@gmail.com>
2023-05-18 13:17:34 -0300

    alterados

--- src/components/CardLojas/index.jsx
+++ src/components/CardLojas/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './style.css'
 
-export default function CardLojas() {
+export default function CardLojas({nome, id}) {
   return (
-    <div className='containerLoja'>
-        <div className='loja'>
-            <p className='nomeLoja'>1-Loja</p>
+    <div className='containerLoja' id={id}>
+        <div className='loja' >
+            <p className='nomeLoja'>{nome}</p>
             <input className='check' type="checkbox" name="" id="" />
         </div>
     </div>
