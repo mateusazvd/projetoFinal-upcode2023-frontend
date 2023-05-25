a30db724d5befb4b7e2fdd3fef1851e79f14d261
carlosXavierrr <eduardoxavier2711@gmail.com>
2023-05-25 13:50:30 -0300

    alterações nas flash alert's

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -47,41 +47,108 @@ export default function Button() {
     return ""
   }
 
-  
+  //Flash alert para caso não preencha lojas
+  function erroLojaVazia() {
+    toast.warning('Preencha corretamente as lojas', {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    })
+  };
 
+  //Flash alert para caso não preencha corretamente produtos
+  function erroProdutosVazia() {
+    toast.warning('Preencha corretamente os produtos', {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
 
-function PostForm() {
-  let newListaProduto = getProdutosPorID()
-  let newLojas = lojas?.map(item => item.toString())
-  let form = {
-    "lojas": newLojas,
-    "produtos": newListaProduto,
-    "categoria": categoria,
-    "dataInicio": formataDataIso(dataInicio),
-    "dataFinal": formataDataIso(dataFinal)
+    })
+  };
+
+  //caso não preencha corretamente as categorias
+  function erroCategoriasVazia() {
+    toast.warning('Preencha corretamente as categorias', {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+
+    })
+  };
+  //caso não preencha corretamente os periodos
+  function NotificacaoPeriodo() {
+    toast.warning('Preencha corretamente o período', {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    })
   };
 
 
+  function PostForm() {
+    let newListaProduto = getProdutosPorID()
+    let newLojas = lojas?.map(item => item.toString())
+    let form = {
+      "lojas": newLojas,
+      "produtos": newListaProduto,
+      "categoria": categoria,
+      "dataInicio": formataDataIso(dataInicio),
+      "dataFinal": formataDataIso(dataFinal)
+    };
 
-  
-  
-    fetch('https://api-aspnet-final-production.up.railway.app/api/pesquisa/cadastro', {
-      method: 'POST',
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify(form)
-
-    }).then(() => console.log(form, 'Cadastrado com sucesso')).catch(e => console.log(e, "erro amigao"))
-  
 
 
-}
+    if (lojas.length == 0) {
+      return erroLojaVazia();
+    }
+    else if (categoria.value == 0) {
+      return erroCategoriasVazia()
+    }
+    else if (dataInicio == 0 || dataFinal == 0) {
+      return NotificacaoPeriodo()
+    }
+    else if (produtos.length == 0) {
+      return erroProdutosVazia()
+    }
+    else {
+      fetch('https://api-aspnet-final-production.up.railway.app/api/pesquisa/cadastro', {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(form)
 
-return (
-  <div className='containerButton'>
-    <button className='button' onClick={() => PostForm()}>Enviar</button>
-    <ToastContainer />
-  </div>
-)
+      }).then(() => console.log(form, 'Cadastrado com sucesso')).catch(e => console.log(e, "erro amigao"))
+    }
+
+
+
+  }
+
+  return (
+    <div className='containerButton'>
+      <button className='button' onClick={() => PostForm()}>Enviar</button>
+      <ToastContainer />
+    </div>
+  )
 }
 
 
