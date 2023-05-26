11ca554f5124fd78fecb39ed1699751b08b782ce
carlosXavierrr <eduardoxavier2711@gmail.com>
2023-05-26 16:34:11 -0300

    add funcionalidades ao dashboard

--- src/components/Dashboard/MainDashboard/index.jsx
+++ src/components/Dashboard/MainDashboard/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Filtros from '../FIltros'
-
+import PesqEnviadas from '../PesquisasEnviadas'
 import './style.css'
 
 export default function Dashboard() {
   return (
     <div className='containerMainDashboard' >
         <Filtros />
+        <PesqEnviadas/>
     </div>
   )
 }
