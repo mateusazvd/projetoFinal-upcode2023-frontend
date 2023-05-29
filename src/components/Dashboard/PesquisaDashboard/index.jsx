8f1596d45daf45bec910bb760034d7622347f765
carlosXavierrr <eduardoxavier2711@gmail.com>
2023-05-29 10:35:05 -0300

    alterações dashboard

---
+++ src/components/Dashboard/PesquisaDashboard/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+export default function PesquisaDashboard() {
+  return (
+    <div className='containerPesquisaDash'>
+     <p className='itemPesquisa'>Pesquisa</p>
+    </div>
+  )
+}
